bf8f5f08628b9dc7881ae99e7edc7cd05d4ef72f
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-04-28 10:55:25 +0700

    change first code

---
+++ database/migrations/2023_04_26_183729_create_clients_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_logo')->nullable();
+            $table->string('client_name')->nullable();
+            $table->string('client_id');
+            $table->string('client_key');
+            $table->string('client_type')->nullable(); //ios/android/web
+            $table->string('client_app_key')->nullable(); //
+            $table->string('client_version')->nullable();
+            $table->boolean('client_build_number')->nullable();
+            $table->boolean('client_update_required')->nullable();
+            $table->boolean('locked')->default(0);
+            $table->integer('client_update_at');
+            $table->json('client_option')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_clients');
+    }
+};
--- src/ClientsServiceProvider.php
+++ src/ClientsServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace FireAZ\Boilerplate;
+namespace FireAZ\Clients;
 
 use Illuminate\Support\ServiceProvider;
 use FireAZ\LaravelPackage\ServicePackage;
-use FireAZ\LaravelPackage\WithServiceProvider;
+use FireAZ\Platform\Traits\WithServiceProvider;
 
-class BoilerplateServiceProvider extends ServiceProvider
+class ClientsServiceProvider extends ServiceProvider
 {
     use WithServiceProvider;
 
@@ -17,7 +17,7 @@ class BoilerplateServiceProvider extends ServiceProvider
          *
          */
         $package
-            ->name('boilerplate')
+            ->name('clients')
             ->hasConfigFile()
             ->hasViews()
             ->hasHelpers()
