dbc6e6bf8d74521ddbd3517ca0096b69246ae581
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-04-29 15:49:54 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use FireAZ\Platform\Facades\Jwt;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/abc2', function (Request $request) {
-    return ['appClient' => $request->appClient(),'user' => $request->user()];
+Route::post('/auth/login', function (Request $request) {
+    $credentials = $request->all();
+
+    if (!Auth::validate($credentials)) :
+        return redirect()->to('login')
+            ->withErrors(trans('auth.failed'));
+    endif;
+
+    $user = Auth::getProvider()->retrieveByCredentials($credentials);
+    $exp_at = \time() + 60 * 60 * 24; // 24h
+    return ['appClient' => $request->appClient(), 'user' => $user, 'token' => Jwt::encode($user->toArray(), ['exp' => $exp_at]), 'token_exp' => $exp_at];
 });
